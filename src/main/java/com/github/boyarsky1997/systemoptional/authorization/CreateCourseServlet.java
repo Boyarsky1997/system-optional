c85aff1cad03c7e302eff2c885ab15ba63df5ba4
roman <Boyarsky1997>
2021-03-09 12:46:15 +0200

    Blocked students' access to creating courses

--- src/main/java/com/github/boyarsky1997/systemoptional/authorization/CreateCourseServlet.java
+++ src/main/java/com/github/boyarsky1997/systemoptional/authorization/CreateCourseServlet.java
@@ -1,6 +1,7 @@
 package com.github.boyarsky1997.systemoptional.authorization;
 
 import com.github.boyarsky1997.systemoptional.db.CourseDAO;
+import com.github.boyarsky1997.systemoptional.model.Role;
 import com.github.boyarsky1997.systemoptional.model.User;
 
 import javax.servlet.ServletException;
@@ -15,22 +16,26 @@ public class CreateCourseServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/createCourse.jsp").forward(req, resp);
+        HttpSession session = req.getSession(false);
+        User client = (User) session.getAttribute("client");
+        if (!client.getRole().equals(Role.TEACHER)) {
+            resp.sendError(403);
+            return;
+        } else {
+            req.getRequestDispatcher("/jsp/createCourse.jsp").forward(req, resp);
+        }
     }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        User client = (User) session.getAttribute("client");
         req.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         String description = req.getParameter("description");
         System.out.println(name);
         System.out.println(description);
-        HttpSession session = req.getSession(false);
-        User client = (User) session.getAttribute("client");
         int id = client.getId();
         int lastId = courseDAO.insert(name, description, id);
-        resp.sendRedirect("/course?id="+lastId);
-
-
+        resp.sendRedirect("/course?id=" + lastId);
     }
 }
