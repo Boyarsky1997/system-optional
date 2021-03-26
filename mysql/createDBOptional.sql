f65779b5cbe7e362b312ae502c6921e58418fadc
roman <Boyarsky1997>
2021-03-26 21:31:16 +0200

    initialize database on docker start

---
+++ mysql/createDBOptional.sql
@@ -0,0 +1,57 @@
+create database optional_database;
+use optional_database;
+
+CREATE TABLE user
+(
+    id       INTEGER AUTO_INCREMENT,
+    role     ENUM ('STUDENT','TEACHER'),
+    name     VARCHAR(25) not null,
+    surname  VARCHAR(25) not null,
+    login    VARCHAR(40) NOT NULL unique,
+    password VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE course
+(
+    id         INTEGER AUTO_INCREMENT,
+    title      VARCHAR(25),
+    teacher_id int,
+    description      VARCHAR(225),
+    FOREIGN KEY (teacher_id) references user (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE comment
+(
+    id         INTEGER AUTO_INCREMENT,
+    message    VARCHAR(250),
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES user (id),
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES user (id),
+    date       TIMESTAMP default CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE student_course
+(
+    user_id   INT,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    course_id INT,
+    FOREIGN KEY (course_id) REFERENCES course (id),
+    PRIMARY KEY (user_id, course_id)
+);
+
+INSERT INTO user(role, name, surname, login, password)
+values ('TEACHER','Roman','Boyarsky','roman@gmail.com','123'),
+('STUDENT','Vitalik','Boyarsky','vitalik@gmail.com','123');
+
+INSERT INTO course( title, teacher_id, description)
+VALUES ('English',1,'course of English language');
+
+INSERT INTO  student_course(user_id, course_id)
+VALUES (2,1);
+
+INSERT INTO comment(message, teacher_id, student_id)
+VALUES ('Good job!',1,2);
